cb0e3b933c858d7716b9419644b0542efbb2e8d5
simon-ibh <simon@icebreakerhealth.com>
2016-04-06 11:35:59 -0700

    Initial submission of Gettysburg address homework, practising the use of event listeners and JQuery DOM manipulation

---
+++ app.js
@@ -0,0 +1,88 @@
+$(document).ready(function() {
+
+  // Reveal the contents of the page that are initially hidden
+  var $container = $("#container");
+
+  $container
+    .fadeIn(2000);
+
+  // Highlight each paragraph with yellow when hovered over
+
+  $("p").hover(
+      function() {
+          $($(this)).css("background","yellow");
+        },
+      function() {
+        $($(this)).css("background","none");
+      });
+
+  // Set up a click event on every `<h2>` element that simultaneously fades it
+  // to 25 percent opacity while growing its left-hand margin to 20px. Then,
+  // when this animation is complete, fade the speech text to 50 percent
+  // opacity.
+
+  $(document).on("click","h2", function() {
+    $($(this))
+      .fadeTo(1000,0.25, function() {
+      $($(this))
+        .animate({
+            "margin-left": "+=20px"},1000, function() {
+                $(".speech").fadeTo(1000,0.5)
+            });
+    });
+  });
+
+  // **Bonus:** Make the style switcher work.
+
+  $(document).on("click","#switcher-large", function() {
+    $(".speech").css("fontSize","20px");
+  });
+
+  $(document).on("click","#switcher-small", function() {
+    $(".speech").css("fontSize","8px");
+  });
+
+  $(document).on("click","#switcher-default", function() {
+    $(".speech").css("fontSize","12px");
+  });
+
+  // **Challenge:** React to presses of the arrow keys by smoothly moving
+  // the switcher box 20 pixels in the corresponding direction. The key codes
+  // for the arrow keys are: 37 (left), 38 (up), 39 (right), and 40 (down).
+
+  $(document).keydown(function(e) {
+    switch(e.which) {
+        case 37: // left
+        $("#switcher")
+          .animate({
+              "margin-left": "-=20px"},1000
+            );
+        break;
+
+        case 38: // up
+        $("#switcher")
+          .animate({
+              "margin-top": "-=20px"},1000
+            );
+        break;
+
+        case 39: // right
+        $("#switcher")
+          .animate({
+              "margin-left": "+=20px"},1000
+            );
+        break;
+
+        case 40: // down
+        $("#switcher")
+          .animate({
+              "margin-top": "+=20px"},1000
+            );
+        break;
+
+        default: return; // exit this handler for other keys
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+  });
+
+});
